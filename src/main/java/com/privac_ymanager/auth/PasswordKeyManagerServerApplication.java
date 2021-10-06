0f10f4fe95a7a06673d3a6add7ee8fb82433a928
Annie <gavrilita.anastasia@isa.utm.md>
2021-10-06 16:07:01 +0300

    project initiallization

---
+++ src/main/java/com/privac_ymanager/auth/PasswordKeyManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.privac_ymanager.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordKeyManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordKeyManagerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/privac_ymanager/auth/PasswordKeyManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privac_ymanager.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordKeyManagerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
